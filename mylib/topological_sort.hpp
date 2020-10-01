d74190b07c2265199f205f2805a6dbb99955980a
hareku <hareku908@gmail.com>
2020-10-02 00:25:25 +0900

    Add topological_sort

---
+++ mylib/topological_sort.hpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+namespace mylib {
+
+struct topological_sort_graph {
+  public:
+    topological_sort_graph() : _n(0) {}
+    topological_sort_graph(int n) : _n(n), g(n), indegree(n, 0) {}
+
+    void add_edge(int from, int to) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        g[from].push_back(_edge{to});
+        ++indegree[to];
+    }
+
+    std::vector<int> topological_sort() {
+        std::stack<int> st;
+        for(int i = 0; i < _n; ++i) {
+            if(indegree[i] == 0) {
+                st.push(i);
+            }
+        }
+
+        std::vector<int> res;
+
+        while(!st.empty()) {
+            int v = st.top();
+            st.pop();
+
+            res.push_back(v);
+
+            for(_edge& e : g[v]) {
+                --indegree[e.to];
+                if(indegree[e.to] == 0) {
+                    st.push(e.to);
+                }
+            }
+        }
+
+        return res;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to;
+    };
+    std::vector<std::vector<_edge>> g;
+    std::vector<int> indegree;
+};
+
+}  // namespace mylib
---
+++ mylib/topological_sort_test.cpp
@@ -0,0 +1,24 @@
+// TODO: https://github.com/online-judge-tools/verification-helper/issues/30
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B"
+#include <bits/stdc++.h>
+#include "topological_sort.hpp"
+using namespace std;
+
+int main() {
+    int N,M; cin >> N >> M;
+
+    mylib::topological_sort_graph g(N);
+
+    for(int i = 0; i < M; ++i) {
+        int a,b; cin >> a >> b;
+        g.add_edge(a, b);
+    }
+
+    auto result = g.topological_sort();
+
+    for(auto v : result) {
+        cout << v << endl;
+    }
+
+    return 0;
+}
