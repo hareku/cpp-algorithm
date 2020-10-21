5fe34f3b51090190ae150a148eb0a01f34f148f5
hareku <hareku908@gmail.com>
2020-10-21 12:16:04 +0000

    Tweak main template

--- main.template.cpp
+++ main.template.cpp
@@ -3,8 +3,6 @@
 #include "utils/debug.hpp"
 // #include <atcoder/all>
 using namespace std;
-// const long long MOD = 1e9+7;
-// const long long MOD = 998244353;
 
 int main() {
     std::cin.tie(0);
