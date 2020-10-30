b9989c0c2871d50d500ffb53eacef1187548a340
hareku <hareku908@gmail.com>
2020-10-30 14:03:38 +0000

    Add lca

---
+++ test/graph/lca.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+#include <bits/stdc++.h>
+#include "../../lib/graph/lca.hpp"
+using namespace std;
+
+int main() {
+    int N,Q; cin >> N >> Q;
+    lib::graph::lca_tree tree(N);
+
+    for(int i = 1; i < N; ++i) {
+        int p; cin >> p;
+        tree.add_edge(p, i);
+    }
+
+    tree.build();
+    for(int i = 0; i < Q; ++i) {
+        int a,b; cin >> a >> b;
+        cout << tree.lca(a, b) << endl;
+    }
+    return 0;
+}
