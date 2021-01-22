daf24cd7741f862b500c84bd667b29fe129a29ab
hareku <hareku908@gmail.com>
2021-01-22 05:41:41 +0000

    tweak arg naming

--- lib/sequence/lis.hpp
+++ lib/sequence/lis.hpp
@@ -5,14 +5,14 @@
 
 namespace lib::sequence {
 
-template <class T> int lis(std::vector<T>& a, bool broad = true) {
+template <class T> int lis(std::vector<T>& a, bool can_weakly_increase = true) {
     const T inf = std::numeric_limits<T>::max();
 
     int n = int(a.size());
     std::vector<T> dp(n, inf);
 
     for (int i = 0; i < n; ++i) {
-        if(broad) {
+        if(can_weakly_increase) {
             *std::upper_bound(dp.begin(), dp.end(), a[i]) = a[i];
         } else {
             *std::lower_bound(dp.begin(), dp.end(), a[i]) = a[i];
