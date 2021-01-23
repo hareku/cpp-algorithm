72112f750b86abfe43ff20c0678514f765d77b3b
hareku <hareku908@gmail.com>
2021-01-23 14:43:18 +0000

    add matrix lib

---
+++ lib/math/matrix.hpp
@@ -0,0 +1,98 @@
+#ifndef LIB_MMTH_MMTRIX_HPP
+#define LIB_MMTH_MMTRIX_HPP 1
+
+#include <bits/stdc++.h>
+
+namespace lib::math {
+
+template <class T> struct Matrix {
+  public:
+    std::vector<std::vector<T>> M;
+
+    Matrix() {}
+    Matrix(int n, int m) : M(n, std::vector<T>(m, 0)) {}
+    Matrix(int n) : M(n, std::vector<T>(n, 0)) {};
+
+    int height() const {
+        return (int) M.size();
+    }
+
+    int width() const {
+        return (int) M[0].size();
+    }
+
+    inline const std::vector<T> &operator[](int k) const {
+        return (M.at(k));
+    }
+
+    inline std::vector<T> &operator[](int k) {
+        return (M.at(k));
+    }
+
+    static Matrix I(int n) {
+        Matrix mat(n);
+        for(int i = 0; i < n; i++) mat[i][i] = 1;
+        return (mat);
+    }
+
+    Matrix &operator+=(const Matrix &B) {
+        int n = height(), m = width();
+        assert(n == B.height() && m == B.width());
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                (*this)[i][j] += B[i][j];
+        return (*this);
+    }
+
+    Matrix &operator-=(const Matrix &B) {
+        int n = height(), m = width();
+        assert(n == B.height() && m == B.width());
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                (*this)[i][j] -= B[i][j];
+        return (*this);
+    }
+
+    Matrix &operator*=(const Matrix &B) {
+        int n = height(), m = B.width(), p = width();
+        assert(p == B.height());
+        std::vector< std::vector<T> > C(n, std::vector<T>(m, 0));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                for(int k = 0; k < p; k++)
+                    C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+        M.swap(C);
+        return (*this);
+    }
+
+    Matrix &operator^=(long long k) {
+        Matrix B = Matrix::I(height());
+        while(k > 0) {
+            if(k & 1) B *= *this;
+            *this *= *this;
+            k >>= 1LL;
+        }
+        M.swap(B.M);
+        return (*this);
+    }
+
+    Matrix operator+(const Matrix &B) const {
+        return (Matrix(*this) += B);
+    }
+
+    Matrix operator-(const Matrix &B) const {
+        return (Matrix(*this) -= B);
+    }
+
+    Matrix operator*(const Matrix &B) const {
+        return (Matrix(*this) *= B);
+    }
+
+    Matrix operator^(const long long k) const {
+        return (Matrix(*this) ^= k);
+    }
+};
+
+}    // namespace lib::math
+
+#endif    // LIB_MMTH_MMTRIX_HPP
---
+++ test/math/matrix.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_7_D"
+#include <bits/stdc++.h>
+#include "../../lib/math/matrix.hpp"
+using namespace std;
+
+int main() {
+    int N,M,L; cin >> N >> M >> L;
+
+    lib::math::Matrix<long long> A(N, M), B(M, L);
+
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < M; ++j) {
+            cin >> A.M[i][j];
+        }
+    }
+
+    for(int i = 0; i < M; ++i) {
+        for(int j = 0; j < L; ++j) {
+            cin >> B.M[i][j];
+        }
+    }
+
+    A *= B;
+
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < L; ++j) {
+            cout << A.M[i][j];
+            if(j + 1 < L) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
