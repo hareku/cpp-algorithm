11339aba3601b774d7698926273dd8cabe6abfa7
hareku <hareku908@gmail.com>
2021-01-30 11:22:10 +0000

    add debug util for atcoder

---
+++ utils/atcoder_debug.hpp
@@ -0,0 +1,9 @@
+#include <atcoder/all>
+
+string to_string(atcoder::modint998244353 m) {
+    return to_string(m.val());
+}
+
+string to_string(atcoder::modint1000000007 m) {
+    return to_string(m.val());
+}
