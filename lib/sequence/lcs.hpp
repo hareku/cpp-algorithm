4e7fd3350a34ea0d0afff02a8f034ce0643fcfbf
hareku <hareku908@gmail.com>
2021-02-18 07:03:34 +0000

    clean lcs

--- lib/sequence/lcs.hpp
+++ lib/sequence/lcs.hpp
@@ -9,17 +9,17 @@ template <class T> int lcs(std::vector<T>& s1, std::vector<T>& s2) {
     int n1 = s1.size(), n2 = s2.size();
     std::vector<std::vector<int>> dp(n1 + 1, std::vector<int>(n2 + 1, 0));
 
-    for(int i = n1 - 1; i >= 0; --i) {
-        for(int j = n2 - 1; j >= 0; --j) {
+    for(int i = 0; i < n1; ++i) {
+        for(int j = 0; j < n2; ++j) {
             if(s1[i] == s2[j]) {
-                dp[i][j] = dp[i + 1][j + 1] + 1;
+                dp[i + 1][j + 1] = dp[i][j] + 1;
             } else {
-                dp[i][j] = std::max(dp[i + 1][j], dp[i][j + 1]);
+                dp[i + 1][j + 1] = std::max(dp[i + 1][j], dp[i][j + 1]);
             }
         }
     }
 
-    return dp[0][0];
+    return dp[n1][n2];
 }
 
 }  // namespace lib::sequence
