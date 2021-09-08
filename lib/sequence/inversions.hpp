6bc0493e507cb0af56f4f46a990015b0be87d755
hareku <hareku908@gmail.com>
2021-09-08 15:22:45 +0000

    refactor: inversions

--- lib/sequence/inversions.hpp
+++ lib/sequence/inversions.hpp
@@ -3,31 +3,24 @@
 
 #include <bits/stdc++.h>
 #include "./fenwick_tree.hpp"
+#include "./compressor.hpp"
 
 namespace lib::sequence {
 
 template <class T> long long inversions(std::vector<T>& a) {
+    // compress values for fenwick_tree
+    compressor<T> cmp;
+    for(T v:a) {
+        cmp.add(v);
+    }
+    auto v2i = cmp.compress();
+
     int n = (int) a.size();
-
-    std::set<T> set;
-    for(int i = 0; i < n; ++i) {
-        set.insert(a[i]);
-    }
-    std::unordered_map<T, int> v2i;
-    for(T v:set) {
-        v2i[v] = (int) v2i.size();
-    }
-
-    std::vector<T> b(n);
-    for(int i = 0; i < n; ++i) {
-        b[i] = T(v2i[a[i]]);
-    }
-
     lib::sequence::fenwick_tree<int> ft(n);
     long long count = 0;
     for(int i = 0; i < n; ++i) {
-        ft.add(b[i], 1);
-        count += ft.sum(b[i] + 1, n);
+        ft.add(v2i[a[i]], 1);
+        count += ft.sum(v2i[a[i]] + 1, n);
     }
     return count;
 }
