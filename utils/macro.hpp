9a4e7b8216f852dea8234f813f73a1d10d88dbe2
hareku <hareku908@gmail.com>
2021-11-08 21:17:19 +0900

    add macro

--- utils/macro.hpp
+++ utils/macro.hpp
@@ -31,6 +31,8 @@ template<typename T> void printd(deque<T>&v) {
 }
 const int dy[4] = { 0, 1, 0, -1 };
 const int dx[4] = { 1, 0, -1, 0 };
+const int dx2[4] = { 1, 1, -1, -1 };
+const int dy2[4] = { 1, -1, 1, -1 };
 const int dy8[8] = { 1, 1, 1, 0, 0, -1, -1, -1 };
 const int dx8[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 double deg2rad(double deg) { return deg * M_PI / 180; }
