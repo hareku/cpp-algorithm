3beda39796dbc2d17a85d43e39a153c8e8da86e3
hareku <hareku908@gmail.com>
2023-10-09 12:42:28 +0900

    update template

--- main.template.cpp
+++ main.template.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 #include "utils/macro.hpp"
+#include "utils/atcoder_debug.hpp"
 #include "utils/debug.hpp"
-// #include <atcoder/all>
+#include <atcoder/all>
 using namespace std;
+
+using mint = atcoder::modint;
+// using mint = atcoder::modint1000000007;
+// const long long MOD = 1e9+7;
+// using mint = atcoder::modint998244353;
+// const long long MOD = 998244353;
+
+using pqt = int;
+auto pqcomp = [](pqt a, pqt b) {
+    return a < b;
+};
+std::priority_queue<pqt, std::vector<pqt>, decltype(pqcomp)> que{pqcomp};
 
 int main() {
     std::cin.tie(0);
     std::ios_base::sync_with_stdio(false);
+
 
 }
