436263a86089dddf6efec94c0c58df8bdedd5c4f
hareku <hareku908@gmail.com>
2025-07-12 12:59:50 +0900

    update manacher

--- lib/string/manacher.hpp
+++ lib/string/manacher.hpp
@@ -6,7 +6,7 @@
 namespace lib::string {
 
 // O(S.size())
-// R[i] = iを中心とした回分の長さの半径
+// R[i] = iを中心とした回文の長さの半径, 直径をkとして(k+1)/2
 // ref: https://snuke.hatenablog.com/entry/2014/12/02/235837
 template <class T> std::vector<int> manacher(const std::vector<T> &S) {
     std::vector<int> R(S.size());
@@ -19,6 +19,15 @@ template <class T> std::vector<int> manacher(const std::vector<T> &S) {
         i += k; j -= k;
     }
     return R;
+}
+
+std::vector<int> manacher(const std::string& s) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return manacher(s2);
 }
 
 }  // namespace lib::string
