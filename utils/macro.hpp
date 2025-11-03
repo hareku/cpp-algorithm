283734cca79b0728ad5045d7e4d34b731db5c818
hareku <hareku908@gmail.com>
2025-11-03 13:27:15 +0900

    feat: hash_combine

--- utils/macro.hpp
+++ utils/macro.hpp
@@ -87,3 +87,9 @@ struct compare_first {
         return a.first < b.first;
     }
 };
+
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v) {
+    std::size_t h = std::hash<T>{}(v);
+    seed ^= h + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
